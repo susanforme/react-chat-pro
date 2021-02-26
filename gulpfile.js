084902f900411692d0d0e796b7fe96f80b33339d
Spring  Breeze <rzc307853639@gmail.com>
2021-02-26 21:21:20 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,7 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const del = require('del');
 
-// const paths = {
-//   dest: {
-//     lib: 'lib', // commonjs 文件存放的目录名 - 本块关注
-//     esm: 'esm', // ES module 文件存放的目录名 - 暂时不关心
-//     dist: 'dist', // umd文件存放的目录名 - 暂时不关心
-//   },
-//   styles: 'src/**/*.less', // 样式文件路径 - 暂时不关心
-// };
 function compileScripts(babelEnv, destDir) {
   // 设置环境变量
   process.env.BABEL_ENV = babelEnv;
@@ -24,10 +17,15 @@ function copyCssTask() {
 }
 
 function copyDTs() {
-  return gulp.src(['/dist/src/*']).pipe(gulp.dest('dist'));
+  return gulp.src('./dist/src/**/*.*').pipe(gulp.dest('dist'));
+}
+
+function cleanD(cb) {
+  return del(['dist/src', 'yarn-error.log'], cb);
 }
 
 module.exports = {
   build: gulp.parallel(tsTask, copyCssTask),
   copyDTs,
+  cleanD,
 };
